880abe660b313f482cbb635e198ccab495a39e61
evg <optix500@gmail.com>
2026-09-20 16:20:16 +0300

    Update Module.js

--- Module.js
+++ Module.js
@@ -8,7 +8,7 @@ Ext.define('Store.passenger_transit.Module', {
     extensionName: 'passenger_transit',
 
     // URL Node.js бэкенда
-    backendBaseUrl: 'https://pillow-extremely-cakes-conducted.trycloudflare.com',
+    backendBaseUrl: 'https://administrative-removable-middle-vinyl.trycloudflare.com',
 
     getBackendUrl: function(action) {
         return this.backendBaseUrl + '/api/' + action;
