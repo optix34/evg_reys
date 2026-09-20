eafe5cc6b996fead9b6403aa3f97718b068631b0
evg <optix500@gmail.com>
2026-09-20 11:54:11 +0300

    Fix formatting of backendBaseUrl in Module.js

--- Module.js
+++ Module.js
@@ -8,7 +8,7 @@ Ext.define('Store.passenger_transit.Module', {
     extensionName: 'passenger_transit',
 
     // URL Node.js бэкенда
-    backendBaseUrl: ' https://allowed-face-transparency-grew.trycloudflare.com',
+    backendBaseUrl: 'https://allowed-face-transparency-grew.trycloudflare.com',
 
     getBackendUrl: function(action) {
         return this.backendBaseUrl + '/api/' + action;
